360e0379d61077d29e0e042a73743dc23f95fc28
Motya98 <113919191+Motya98@users.noreply.github.com>
2023-04-09 11:26:20 +0500

    Add files via upload

---
+++ kontur_2_1.py
@@ -0,0 +1,17 @@
+y = int(input())
+m = []
+for i in range(y):
+    m.append(int(input()))
+z = []
+print(m)
+while True:
+    if len(m) == 1:
+        print(-m[0])
+        break
+    for j in m:
+        for i in m:
+            if j + i == 0:
+                z.append(j)
+                z.append(i)
+                m.remove(j)
+                m.remove(i)
