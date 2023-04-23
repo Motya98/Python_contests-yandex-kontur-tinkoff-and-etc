afa70eb78f7d7364ef337231c1658ae5053b0bb7
Motya98 <113919191+Motya98@users.noreply.github.com>
2023-04-23 12:25:58 +0500

    Update yandex_3_1.py

--- yandex_3_1.py
+++ yandex_3_1.py
@@ -13,8 +13,6 @@ for j in range(n):
         koordinaty_1.append((j + 1, i + 1)) 
 luchee_1 = max(spisok_1)
 days_1 = koordinaty_1[spisok_1.index(luchee_1)]
-
-
 if n >= 4:
     for j in range(n):
         s_pokupki_1 = 1 / prices[j]
@@ -25,13 +23,12 @@ if n >= 4:
                 for ii in range(jj + 1, n, 1):
                     s_prodagi_2 = s_pokupki_2 * prices[ii]
                     spisok_2.append(s_prodagi_2)
-                    koordinaty_2.append((j + 1, i + 1, jj + 1, ii + 1))
+                    koordinaty_2.append((j + 1, i + 1, jj + 1, ii + 1))                    
 if len(spisok_2) == 0: 
     spisok_2.append(-1)
     koordinaty_2.append((0, 0))
 luchee_2 = max(spisok_2)
 days_2 = koordinaty_2[spisok_2.index(luchee_2)]
-
 if luchee_1 > luchee_2 and luchee_1 > 1:
     print(1)
     print(days_1[0], days_1[1])
